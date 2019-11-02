8d70f8a64fcc979467e32195418933774403e276
Stas Kobzar <stas@modulis.ca>
2019-11-02 16:01:24 -0400

    Setup fuzz tests

---
+++ sipmsg/fuzz.go
@@ -0,0 +1,15 @@
+// +build gofuzz
+package sipmsg
+
+func Fuzz(data []byte) int {
+	msg, err := MsgParse(data)
+	if err != nil {
+		if msg != nil {
+			panic("msg != nil on error")
+		}
+		return 0
+	}
+
+	_ := msg.String()
+	return 1
+}
--- sipmsg/go.mod
+++ sipmsg/go.mod
@@ -2,4 +2,7 @@ module github.com/staskobzar/gosip/sip
 
 go 1.12
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/dvyukov/go-fuzz v0.0.0-20191022152526-8cb203812681 // indirect
+	github.com/stretchr/testify v1.4.0
+)
