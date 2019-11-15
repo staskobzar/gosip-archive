747e4fc045a18c76e032d52bc892e7bb0d0e373d
Stas Kobzar <stas@modulis.ca>
2019-11-14 21:07:17 -0500

    Key/value fields builder.

--- sdp/builder.go
+++ sdp/builder.go
@@ -1,3 +1,142 @@
 package sdp
 
+import (
+	"bytes"
+	"strconv"
+	"time"
+)
 
+// NewMessage initiates new SDP Message
+func NewMessage(host string) *Message {
+	id := idFromNTP()
+	msg := &Message{
+		mediaIdx: -1,
+		ver:      0,
+		Origin: Origin{
+			username: []byte{'-'},
+			sessID:   id,
+			sessVer:  id,
+			netType:  []byte("IN"),
+			addrType: []byte("IP4"),
+			unicAddr: []byte(host),
+		},
+		subject: []byte{'-'},
+	}
+
+	return msg
+}
+
+// String SDP Message as string
+func (m *Message) String() string {
+	var b buffer
+
+	b.compile(m)
+	return b.String()
+}
+
+// SetSubject session subject s=* field set
+func (m *Message) SetSubject(subj string) {
+	m.subject = []byte(subj)
+}
+
+// SetInfo session info i=* field set
+func (m *Message) SetInfo(info string) {
+	m.info = []byte(info)
+}
+
+// SetURI session uri u=* field set
+func (m *Message) SetURI(uri string) {
+	m.uri = []byte(uri)
+}
+
+// private methods
+func idFromNTP() []byte {
+	n := time.Now().Unix()
+	s := strconv.FormatInt(n, 10)
+	return []byte(s)
+}
+
+type buffer struct {
+	bytes.Buffer
+}
+
+// compile SDP message to buffer
+func (b *buffer) compile(m *Message) {
+	// Session description
+	b.kv('v', []byte{m.ver + 0x30}) // v=  (protocol version)
+	b.o(m)                          // o=  (originator and session identifier)
+	b.kv('s', m.subject)            // s=  (session name)
+	b.kv('i', m.info)               // i=* (session information)
+	b.kv('u', m.uri)                // u=* (URI of description)
+	// e=* (email address)
+	// p=* (phone number)
+	// c=* (connection information -- not required if included in all media)
+	// b=* (zero or more bandwidth information lines)
+
+	// One or more time descriptions ("t=" and "r=" lines; see below)
+	// Time description
+	// t=  (time the session is active)
+	// r=* (zero or more repeat times)
+	b.t(m)
+	// z=* (time zone adjustments)
+	// k=* (encryption key)
+	// a=* (zero or more session attribute lines)
+	// Zero or more media descriptions
+
+	// Media description, if present
+	// m=  (media name and transport address)
+	// i=* (media title)
+	// c=* (connection information -- optional if included at
+	//      session level)
+	// b=* (zero or more bandwidth information lines)
+	// k=* (encryption key)
+	// a=* (zero or more media attribute lines)
+}
+
+// write key/value field
+func (b *buffer) kv(key byte, val []byte) {
+	if len(val) == 0 {
+		return
+	}
+	b.WriteByte(key)
+	b.WriteByte('=')
+	b.Write(val)
+	b.crlf()
+}
+
+// origin o=
+func (b *buffer) o(m *Message) {
+	b.WriteString("o=")
+	b.writeWSpace(m.Origin.username)
+	b.writeWSpace(m.Origin.sessID)
+	b.writeWSpace(m.Origin.sessVer)
+	b.writeWSpace(m.Origin.netType)
+	b.writeWSpace(m.Origin.addrType)
+	b.Write(m.Origin.unicAddr)
+	b.crlf()
+}
+
+// timing t=
+func (b *buffer) t(m *Message) {
+	if len(m.Time) == 0 {
+		b.kv('t', []byte("0 0"))
+		return
+	}
+
+	for _, t := range m.Time {
+		b.WriteString("t=")
+		b.writeWSpace(t.start)
+		b.Write(t.stop)
+		b.crlf()
+	}
+}
+
+// write CRLF
+func (b *buffer) crlf() {
+	b.WriteString("\r\n")
+}
+
+func (b *buffer) writeWSpace(data []byte) {
+	b.Write(data)
+	b.WriteByte(' ')
+}
--- sdp/builder_test.go
+++ sdp/builder_test.go
@@ -1,10 +1,34 @@
 package sdp
 
-func TestBuildNoMedia(t *testing.T) {
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildInit(t *testing.T) {
+	id := string(idFromNTP())
 	str := "v=0\r\n" +
-		"o=alice 2890844526 2890844526 IN IP4 host.atlanta.example.com\r\n" +
-		"s=\r\n" +
-		"c=IN IP4 host.atlanta.example.com\r\n" +
+		"o=- " + id + " " + id + " IN IP4 atlanta.com\r\n" +
+		"s=-\r\n" +
 		"t=0 0\r\n"
-	msg, err := 
+
+	msg := NewMessage("atlanta.com")
+	assert.NotNil(t, msg)
+
+	assert.Equal(t, str, msg.String())
+}
+
+func TestBuildSubjectInfoUri(t *testing.T) {
+	str := "s=Online training\r\n" +
+		"i=SIP and SDP with certification\r\n" +
+		"u=http://lib.sip.com/2019-11-02/readme.pdf\r\n" +
+		"t=0 0\r\n"
+	msg := NewMessage("192.168.1.1")
+	assert.NotNil(t, msg)
+
+	msg.SetSubject("Online training")
+	msg.SetInfo("SIP and SDP with certification")
+	msg.SetURI("http://lib.sip.com/2019-11-02/readme.pdf")
+	assert.Contains(t, msg.String(), str)
 }
