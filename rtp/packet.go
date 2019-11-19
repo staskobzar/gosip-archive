545c22e1b5373f0a74ffcc1c933c1672594b4568
Stas Kobzar <stas@modulis.ca>
2019-11-18 21:21:50 -0500

    RTP header encode.

--- rtp/packet.go
+++ rtp/packet.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 )
 
+const minHeaderLen = 12
+
 // ErrorRTPDecode error raised when failed to decode RTP packet
 var ErrorRTPDecode = errors.New("RTP Packet decode failed")
 
@@ -34,17 +36,18 @@ type Header struct {
 	Timestamp uint32   // sampling timestamp
 	SSRC      uint32   // synchronization source
 	CSRC      []uint32 // 0 to 15 of contributing sources
-	Len       int      // RTP header length. Used to start payload position
+}
+
+func NewHeader() *Header {
+	return &Header{Ver: 2}
 }
 
 // Decode reads RTP packet bytes stream to Header struct
-func Decode(data []byte) (*Header, error) {
+func (h *Header) Decode(data []byte) error {
 
-	if len(data) < 12 {
-		return nil, ErrorRTPDecode
+	if len(data) < minHeaderLen {
+		return ErrorRTPDecode
 	}
-
-	h := &Header{}
 
 	h.Ver = data[0] >> 6
 	h.Pad = ((data[0] >> 5) & 0x01) == 1
@@ -56,9 +59,34 @@ func Decode(data []byte) (*Header, error) {
 	h.Timestamp = binary.BigEndian.Uint32(data[4:8])
 	h.SSRC = binary.BigEndian.Uint32(data[8:12])
 
-	h.Len = 12
+	return nil
+}
 
-	return h, nil
+// Encode RTP header to byte array
+func (h *Header) Encode() []byte {
+	buf := make([]byte, minHeaderLen)
+
+	b2i := func(v bool) byte {
+		if v {
+			return 1
+		}
+		return 0
+	}
+
+	// first byte
+	buf[0] = (h.Ver & 0b11) << 6
+	buf[0] |= (b2i(h.Pad) & 1) << 5
+	buf[0] |= (b2i(h.Ext) & 1) << 4
+	buf[0] |= (h.CCount & 0x0f)
+
+	// second byte
+	buf[1] = (b2i(h.Marker) & 1) << 7
+	buf[1] |= (h.PType & 0x7f)
+
+	binary.BigEndian.PutUint16(buf[2:], h.SeqNum)
+	binary.BigEndian.PutUint32(buf[4:], h.Timestamp)
+	binary.BigEndian.PutUint32(buf[8:], h.SSRC)
+	return buf
 }
 
 // Version RTP header version feild
@@ -72,3 +100,35 @@ func (h *Header) PayloadType() int { return int(h.PType) }
 
 // SequenceNumber RTP header sequence number feild
 func (h *Header) SequenceNumber() int { return int(h.SeqNum) }
+
+// Len RTP header sequence number feild
+func (h *Header) Len() int { return minHeaderLen + len(h.CSRC) }
+
+// SetSeqNum set sequence number of the header
+func (h *Header) SetSeqNum(num int) {
+	h.SeqNum = uint16(num)
+}
+
+// SetTimestamp set header timestamp
+func (h *Header) SetTimestamp(tstamp int) {
+	h.Timestamp = uint32(tstamp)
+}
+
+// SetPayloadType set header payload type
+func (h *Header) SetPayloadType(payload int) {
+	h.PType = uint8(payload)
+}
+
+// SetSSRC set header timestamp
+func (h *Header) SetSSRC(tstamp int) {
+	h.SSRC = uint32(tstamp)
+}
+
+// SetPadding set header padding field to True
+func (h *Header) SetPadding() { h.Pad = true }
+
+// SetExtension set header extention field to True
+func (h *Header) SetExtension() { h.Ext = true }
+
+// SetMarker set header marker field to True
+func (h *Header) SetMarker() { h.Marker = true }
--- rtp/packet_test.go
+++ rtp/packet_test.go
@@ -20,9 +20,9 @@ func TestRTPDecode(t *testing.T) {
 		"\xf2\xfa\xf8\xf8\xfa\xde\xf2\xfa\xf8\xf8\xfa\xde"
 
 	data := []byte(str)
-	rtp, err := Decode(data)
+	rtp := NewHeader()
+	err := rtp.Decode(data)
 	assert.Nil(t, err)
-	assert.NotNil(t, rtp)
 
 	assert.Equal(t, 2, rtp.Version())
 	assert.False(t, rtp.Pad)
@@ -34,21 +34,52 @@ func TestRTPDecode(t *testing.T) {
 	assert.Equal(t, uint32(2995562743), rtp.Timestamp)
 	assert.Equal(t, uint32(3575866327), rtp.SSRC)
 
-	assert.Equal(t, 160, len(data[rtp.Len:]))
+	assert.Equal(t, 12, rtp.Len())
+	assert.Equal(t, 160, len(data[rtp.Len():]))
 
-	rtp, err = Decode([]byte{0x80, 0x81, 0x82})
+	err = rtp.Decode([]byte{0x80, 0x81, 0x82})
 	assert.NotNil(t, err)
-	assert.Nil(t, rtp)
 }
 
-/*
 func TestRTPEncode(t *testing.T) {
-	bin := []byte{0x80, 0xe3, 0x5d, 0x25, 0x00, 0x00, 0x03, 0xc0, 0x04, 0x3e, 0xee, 0x04}
+	bin := []byte{0x80, 0x00, 0x92, 0xe8, 0x00, 0x00, 0x08, 0xc0, 0x34, 0x3d, 0xa9, 0x9b}
 
-	hdr := Encode()
+	hdr := NewHeader()
+	hdr.SetSeqNum(37608)
+	hdr.SetTimestamp(2240)
+	hdr.SetSSRC(0x343da99b)
+	enc := hdr.Encode()
 
-	assert.Equal(bin,hdr)
+	assert.Equal(t, bin, enc)
 }
-*/
 
+func TestRTPCSRC(t *testing.T) {
+	hdr := NewHeader()
+	hdr.SetSeqNum(235)
+	hdr.SetTimestamp(2995562743)
+	hdr.SetSSRC(876456347)
+
+	hdr.SetPadding()
+	hdr.SetExtension()
+	hdr.SetMarker()
+
+	hdr.SetPayloadType(9)
+
+	data := hdr.Encode()
+
+	rtp := NewHeader()
+	err := rtp.Decode(data)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, rtp.Version())
+	assert.True(t, rtp.Pad)
+	assert.True(t, rtp.Ext)
+	assert.Equal(t, 0, rtp.CSRCCount())
+	assert.True(t, rtp.Marker)
+	assert.Equal(t, 9, rtp.PayloadType())
+	assert.Equal(t, 235, rtp.SequenceNumber())
+	assert.Equal(t, uint32(2995562743), rtp.Timestamp)
+	assert.Equal(t, uint32(876456347), rtp.SSRC)
+}
+
+// TODO: CSRC identifiers
 // TODO: RTCPRead
