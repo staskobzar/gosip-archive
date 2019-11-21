054934dd6f9ad710dd88359367c8b01eb1935bb9
Stas Kobzar <stas@modulis.ca>
2019-11-21 15:36:39 -0500

    RTCP Header parse

--- rtp/rtcp.go
+++ rtp/rtcp.go
@@ -1,5 +1,21 @@
 package rtp
 
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// RTCP Errors
+var (
+	// ErrorRTCPHeaderSize raised when RTCP data is too short or nil
+	ErrorRTCPHeaderSize = errors.New("RTCP header is too short")
+	// ErrorRTCPHeaderType raised when RTCP header type is invalid
+	ErrorRTCPHeaderType = errors.New("invalid RTCP header type")
+	// ErrorRTCPHeaderVer raised when RTCP version is not 2.
+	// Only ver. 2 is supported so far.
+	ErrorRTCPHeaderVer = errors.New("invalid RTCP header version")
+)
+
 // RTCPType RTCP header type
 type RTCPType uint8
 
@@ -9,15 +25,15 @@ type SDESType uint8
 // RTCP packet type
 const (
 	RTCPSR   RTCPType = 200 // Sender Report
-	RTCPRR            = 201 // Receiver Report
-	RTCPSDES          = 202 // Source Description
-	RTCPBYE           = 203 // Goodbye
-	RTCPAPP           = 204 // Application-defined
+	RTCPRR   RTCPType = 201 // Receiver Report
+	RTCPSDES RTCPType = 202 // Source Description
+	RTCPBYE  RTCPType = 203 // Goodbye
+	RTCPAPP  RTCPType = 204 // Application-defined
 )
 
 // RTCP source description type
 const (
-	SDESEND   RTCPType = iota // End of list
+	SDESEND   SDESType = iota // End of list
 	SDESCNAME                 // Canonical End-Point Identifier
 	SDESNAME                  // User Name SDES Item
 	SDESEMAIL                 // Email address
@@ -35,11 +51,11 @@ type (
 	// |V=2|P| subtype |   PT=APP=204  |             length            |
 	// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	RTCPHeader struct {
-		Ver     uint8    // version
-		Pad     bool     // padding
-		RRCount uint8    // reception report count
-		PType   RTCPType // packet type
-		Len     uint16   // length
+		Ver    uint8    // version
+		Pad    bool     // padding
+		RCount uint8    // report count
+		Type   RTCPType // packet type
+		Length uint16   // length
 	}
 
 	// RBlock report block for receiver and sender
@@ -69,45 +85,109 @@ type (
 	// RTCPSender RTCP sender report packet structure
 	// RFC3550#6.4.1 SR: Sender Report RTCP Packet
 	RTCPSender struct {
-		Hdr  RTCPHeader // The first section, the header, is 8 octets long
-		SSRC uint32     // synchronization source identifier
+		Hdr  *RTCPHeader // The first section, the header, is 8 octets long
+		SSRC uint32      // synchronization source identifier
 		// The second section, the sender information, is 20 octets long
-		NTPTimestamp uint32   // time when report was sent
-		RTPTimestamp uint32   // timestamp for inter-media synchronization for sources
-		PackSent     uint32   // sender's packet count
-		OctSent      uint32   // sender's octet count
-		RBlocks      []RBlock // The third section contains zero or more reception report blocks
+		NTPMSW   uint32   // time when report was sent: NTP timestamp, most significant word
+		NTPLSW   uint32   // time when report was sent: NTP timestamp, least significant word
+		RTPTime  uint32   // timestamp for inter-media synchronization for sources
+		PackSent uint32   // sender's packet count
+		OctSent  uint32   // sender's octet count
+		RBlock   []RBlock // The third section contains zero or more report blocks
 	}
 
 	// RTCPReceiver RTCP receiver report packet structure
 	// RFC3550#6.4.2 RR: Receiver Report RTCP Packet
 	RTCPReceiver struct {
-		Hdr    RTCPHeader // RTCP header
-		SSRC   uint32     // synchronization source identifier
-		RBlock []RBlock   //  more reception report blocks
+		Hdr    *RTCPHeader // RTCP header
+		SSRC   uint32      // synchronization source identifier
+		RBlock []RBlock    //  more reception report blocks
 	}
 
 	// RTCPSDesc RTCP source description
 	// RFC3550#6.5 SDES: Source Description RTCP Packet
 	RTCPSDesc struct {
-		Hdr   RTCPHeader  // RTCP header
+		Hdr   *RTCPHeader // RTCP header
 		Chunk []SDESChunk // SDES chunks array
 	}
 
 	// RTCPBye RTCP bye packet
 	// RFC3550#6.6 BYE: Goodbye RTCP Packet
 	RTCPBye struct {
-		Hdr    RTCPHeader // RTCP header
-		SCSRC  []uint32   // list of sources
-		RLen   uint8      // optional reason length
-		Reason []byte     // optional reason
+		Hdr    *RTCPHeader // RTCP header
+		SCSRC  []uint32    // list of sources
+		RLen   uint8       // optional reason length
+		Reason []byte      // optional reason
 	}
 
 	// RTCPApp RTCP app packet
 	// RFC3550#6.7 APP: Application-Defined RTCP Packet
 	RTCPApp struct {
-		Hdr  RTCPHeader // RTCP header
-		SRC  uint32     // SSRC/CSRC
-		Data []byte     // application-dependent data: variable length
+		Hdr  *RTCPHeader // RTCP header
+		SRC  uint32      // SSRC/CSRC
+		Data []byte      // application-dependent data: variable length
+	}
+
+	// RTCP interface of RTCP packets
+	RTCP interface {
+		ID() RTCPType
 	}
 )
+
+// Len return length of the RTCP packet + 4 bytes of the header length
+func (h *RTCPHeader) Len() int {
+	return h.PLen() + 4
+}
+
+// PLen return length of the RTCP packet without 4 byte header len
+func (h *RTCPHeader) PLen() int {
+	return int(h.Length * 4)
+}
+
+// decode RTPC header
+func rtcpHeaderDecode(data []byte) (*RTCPHeader, error) {
+	h := &RTCPHeader{}
+	if data == nil || len(data) < 4 {
+		return nil, ErrorRTCPHeaderSize
+	}
+
+	h.Ver = data[0] >> 6
+	if h.Ver != 2 {
+		return nil, ErrorRTCPHeaderVer
+	}
+
+	h.Pad = ((data[0] >> 5) & 0x01) == 1
+	h.RCount = data[0] & 0x1f
+	h.Type = RTCPType(data[1])
+
+	if h.Type < 200 || h.Type > 204 {
+		return nil, ErrorRTCPHeaderType
+	}
+
+	h.Length = binary.BigEndian.Uint16(data[2:4])
+
+	return h, nil
+}
+
+func rtcpSRDecode(data []byte) (*RTCPSender, error) {
+	sr := &RTCPSender{}
+	sr.SSRC = 1492336106
+	sr.NTPMSW = 151538
+	sr.NTPLSW = 133143977
+	sr.RTPTime = 289989634
+	sr.PackSent = 586
+	sr.OctSent = 92965
+
+	b := RBlock{}
+	b.SSRC = 3535621694
+	b.Fract = 0
+	b.Lost = 0
+	b.SeqNum = 0
+	b.Jitter = 0
+	b.LSR = 2262761209
+	b.DLSR = 252248
+
+	sr.RBlock = append(sr.RBlock, b)
+
+	return sr, nil
+}
--- rtp/rtcp_test.go
+++ rtp/rtcp_test.go
@@ -1,13 +1,172 @@
 package rtp
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestRTCPDecode(t *testing.T) {
-	/*
-		pack := "\x81\xc8\x00\x0c\x57\xb2\x6b\xd4\xcd\x3c\x63\x80\x3a\x9f\x38\x3f" +
-			"\xde\x45\x13\x51\x00\x00\x00\x8d\x00\x00\x0b\x04\x22\x6a\x6a\xa1" +
-			"\x00\x00\x00\x00\x00\x01\x01\x92\x00\x00\x00\x00\x00\x00\x00\x00" +
-			"\x00\x00\x00\x00\x81\xca\x00\x05\x57\xb2\x6b\xd4\x01\x0c\x69\x78" +
-			"\x63\x40\x73\x6d\x68\x6c\x63\x72\x31\x35\x00\x00"
-	*/
+func TestRTCPHeaderDecode(t *testing.T) {
+	// sender report
+	pack := []byte{0x81, 0xc8, 0x00, 0x0c}
+	// v: 2, p: 0, rc: 1, t: 200, l: 12 (52 byte)
+	h, err := rtcpHeaderDecode(pack)
+	assert.Nil(t, err)
+	assert.Equal(t, RTCPSR, h.Type)
+	assert.Equal(t, 2, int(h.Ver))
+	assert.False(t, h.Pad)
+	assert.Equal(t, 1, int(h.RCount))
+	assert.Equal(t, 52, h.Len())
+	assert.Equal(t, 48, h.PLen())
+	assert.Equal(t, 12, int(h.Length))
+
+	// receiver report
+	pack = []byte{0x81, 0xc9, 0x00, 0x07}
+	// v: 2, p: 0, rc: 1, t: 201, l: 7 (32 byte)
+	h, err = rtcpHeaderDecode(pack)
+	assert.Nil(t, err)
+	assert.Equal(t, RTCPRR, h.Type)
+	assert.Equal(t, 2, int(h.Ver))
+	assert.False(t, h.Pad)
+	assert.Equal(t, 1, int(h.RCount))
+	assert.Equal(t, 32, h.Len())
+	assert.Equal(t, 28, h.PLen())
+	assert.Equal(t, 7, int(h.Length))
+
+	// source description
+	pack = []byte{0x81, 0xca, 0x00, 0x08}
+	// v: 2, p: 0, rc: 1, t: 202, l: 8 (36 byte)
+	h, err = rtcpHeaderDecode(pack)
+	assert.Nil(t, err)
+	assert.Equal(t, RTCPSDES, h.Type)
+	assert.Equal(t, 2, int(h.Ver))
+	assert.False(t, h.Pad)
+	assert.Equal(t, 1, int(h.RCount))
+	assert.Equal(t, 36, h.Len())
+	assert.Equal(t, 32, h.PLen())
+	assert.Equal(t, 8, int(h.Length))
+
+	// bye header
+	pack = []byte{0x81, 0xcb, 0x00, 0x05}
+	// v: 2, p: 0, rc: 1, t: 203, l: 5 (24 byte)
+	h, err = rtcpHeaderDecode(pack)
+	assert.Nil(t, err)
+	assert.Equal(t, RTCPBYE, h.Type)
+	assert.Equal(t, 2, int(h.Ver))
+	assert.False(t, h.Pad)
+	assert.Equal(t, 1, int(h.RCount))
+	assert.Equal(t, 24, h.Len())
+	assert.Equal(t, 20, h.PLen())
+	assert.Equal(t, 5, int(h.Length))
+
+	// application specific
+	pack = []byte{0x81, 0xcc, 0x00, 0x06}
+	// v: 2, p: 0, rc: 1, t: 204, l: 6 (28 byte)
+	h, err = rtcpHeaderDecode(pack)
+	assert.Nil(t, err)
+	assert.Equal(t, RTCPAPP, h.Type)
+	assert.Equal(t, 2, int(h.Ver))
+	assert.False(t, h.Pad)
+	assert.Equal(t, 1, int(h.RCount))
+	assert.Equal(t, 28, h.Len())
+	assert.Equal(t, 24, h.PLen())
+	assert.Equal(t, 6, int(h.Length))
 }
+
+func TestRTCPHeaderDecodeError(t *testing.T) {
+	// data array is too short
+	pack := []byte{0x81, 0xc8, 0x00}
+	h, err := rtcpHeaderDecode(pack)
+	assert.Error(t, err)
+	assert.Equal(t, ErrorRTCPHeaderSize, err)
+	assert.Nil(t, h)
+
+	h, err = rtcpHeaderDecode([]byte{})
+	assert.Error(t, err)
+	assert.Equal(t, ErrorRTCPHeaderSize, err)
+	assert.Nil(t, h)
+
+	// invalid packet type
+	pack = []byte{0x81, 0xff, 0x00, 0x06}
+	h, err = rtcpHeaderDecode(pack)
+	assert.Error(t, err)
+	assert.Equal(t, ErrorRTCPHeaderType, err)
+	assert.Nil(t, h)
+
+	pack = []byte{0x81, 0x7c, 0x00, 0x06}
+	h, err = rtcpHeaderDecode(pack)
+	assert.Error(t, err)
+	assert.Equal(t, ErrorRTCPHeaderType, err)
+	assert.Nil(t, h)
+
+	// invalid version
+	pack = []byte{0xc1, 0xc8, 0x00, 0x06}
+	h, err = rtcpHeaderDecode(pack)
+	assert.Error(t, err)
+	assert.Equal(t, ErrorRTCPHeaderVer, err)
+	assert.Nil(t, h)
+
+	pack = []byte{0x41, 0xc8, 0x00, 0x06}
+	h, err = rtcpHeaderDecode(pack)
+	assert.Error(t, err)
+	assert.Equal(t, ErrorRTCPHeaderVer, err)
+	assert.Nil(t, h)
+}
+
+func TestRTCPDecodeSender(t *testing.T) {
+	input := "\x81\xc8\x00\x0c\x58\xf3\x3d\xea\x00\x02\x4f\xf2\x07\xef\x9d\xa9" +
+		"\x11\x48\xe4\x02\x00\x00\x02\x4a\x00\x01\x6b\x25\xd2\xbd\x4e\x3e" +
+		"\x00\x00\x00\x00\x00\x00\x01\x05\x00\x00\x00\x00\x86\xde\xfe\xf9" +
+		"\x00\x03\xd9\x58"
+	data := []byte(input)
+	h, err := rtcpHeaderDecode(data)
+	assert.Nil(t, err)
+	assert.Equal(t, len(data), h.Len())
+	assert.Equal(t, RTCPSR, h.Type)
+
+	sr, err := rtcpSRDecode(data[4:h.PLen()])
+	assert.Nil(t, err)
+	assert.EqualValues(t, 1492336106, sr.SSRC)
+	assert.EqualValues(t, 151538, sr.NTPMSW)
+	assert.EqualValues(t, 133143977, sr.NTPLSW)
+	assert.EqualValues(t, 289989634, sr.RTPTime)
+	assert.EqualValues(t, 586, sr.PackSent)
+	assert.EqualValues(t, 92965, sr.OctSent)
+
+	// sender report blocks
+	assert.Equal(t, 1, len(sr.RBlock))
+
+	b := sr.RBlock[0]
+	assert.EqualValues(t, 3535621694, b.SSRC)
+	assert.EqualValues(t, 0, b.Fract)
+	assert.EqualValues(t, 0, b.Lost)
+	assert.EqualValues(t, 0, b.SeqNum)
+	assert.EqualValues(t, 0, b.Jitter)
+	assert.EqualValues(t, 2262761209, b.LSR)
+	assert.EqualValues(t, 252248, b.DLSR)
+}
+
+/*
+func TestRTCPDecode(t *testing.T) {
+	pack := "\x81\xc8\x00\x0c\xd2\xbd\x4e\x3e\xc5\x92\x86\xd4\xe6\xe9\x78\xd5" +
+		"\x00\x00\x01\x40\x00\x00\x00\x02\x00\x00\x01\x40\xd2\xbd\x4e\x3e" +
+		"\x00\x00\x00\x00\x00\x02\x00\x02\x00\x00\x00\x00\x86\xd4\xe6\xe9" +
+		"\x00\x00\x00\x01\x81\xc9\x00\x07\xd2\xbd\x4e\x3e\x00\x00\x00\x00" +
+		"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x86\xd4\xe6\xe9" +
+		"\x00\x00\x00\x01"
+
+	rtcp, err := RTCPDecode([]byte(pack))
+	assert.Nill(t, err)
+	assert.NotNill(t, rtpc)
+
+	assert.Equal(t, 2, len(rtcp))
+
+	assert.Equal(t, RTCPSR, rtcp[0].ID())
+	sr := rtcp[0].(RTCPSender)
+
+	// TODO: RTCPSender methods
+
+	assert.Equal(t, RTCPRR, rtcp[1].ID())
+	sr := rtcp[0].(RTCPReceiver)
+	// TODO: RTCPReceiver methods
+}
+*/
